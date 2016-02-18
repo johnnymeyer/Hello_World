9cb4256115765f930f5b1e10990990bde8470491
John Meyer <johnny@meyer.net>
2016-02-17 17:09:28 -0700

    added home page

--- Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/MainActivity.java
+++ Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ((ImageView)findViewById(R.id.imageView)).setImageResource(R.drawable.glewfrontpage);
 /////I changed this
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
