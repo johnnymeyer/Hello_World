1549f3289acb3bbe494b754c80382213b9a9e240
edoyle1221 <edoyle1221@gmail.com>
2016-02-19 01:59:39 -0700

    Changed button color per Wellesley's request. Added swipe events to MainActivity page

--- Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/MainActivity.java
+++ Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/MainActivity.java
@@ -6,13 +6,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.Touch;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,51 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    // Left to Right swipe action
+                    if (x2 > x1)
+                    {
+                        Toast output = Toast.makeText(this, "Created By:\nWellesley Shumway"
+                                        + "\nKlenton Stone"
+                                        + "\nJohn Meyer"
+                                        + "\nEdward Doyle",
+                                Toast.LENGTH_SHORT);
+                        output.setGravity(Gravity.CENTER, 0, 200);
+                        output.show();
+                    }
+
+                    // Right to left swipe action
+                    else
+                    {
+                        startActivity(new Intent(MainActivity.this, TableOfContents.class));
+                    }
+
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        onTouchEvent(ev);
+        return super.dispatchTouchEvent(ev);
     }
 
     public void tableOfContents(View v) {
