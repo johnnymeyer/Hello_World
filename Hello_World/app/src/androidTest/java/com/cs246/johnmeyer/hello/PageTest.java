78ba1ddf8572c809f61858447fd785788884b072
KStone18 <kstone378@gmail.com>
2016-02-24 10:12:35 -0700

    Merge branch 'master' of https://github.com/johnnymeyer/Hello_World
    # Conflicts:
    #	Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/MainActivity.java

---
+++ Hello_World/app/src/androidTest/java/com/cs246/johnmeyer/hello/PageTest.java
@@ -0,0 +1,17 @@
+package com.cs246.johnmeyer.hello;
+
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Klenton on 2/24/2016.
+ */
+public class PageTest {
+    public void getMyTitleTest() {
+       // assertNotNull((new Page().getMyTitle()));
+
+    }
+
+    public void setMyTitleTest() {
+        assertNotNull((new Page().getMyTitle()));
+    }
+}
---
+++ Hello_World/app/src/main/java/com/cs246/johnmeyer/hello/Page.java
@@ -0,0 +1,41 @@
+package com.cs246.johnmeyer.hello;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Page extends AppCompatActivity {
+    private String title;
+    private String content;
+    private String description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public String getMyTitle(){
+        return title;
+    }
+
+    public void setMyTitle(String myTitle) {
+
+        title = myTitle;
+
+    }
+}
